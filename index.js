0d2f6f7933681c74fb5328867fcfdb1989b2bf9a
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-11-24 20:39:18 +0000

    index.js cleanups

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-console.log('\x1b[35m Migration starting \x1b[0m');
-
 const fs = require('fs');
 const path = require('path');
-const { migrateSlots } = require('./utils/migrtateSlots/index.js')
+const { migrateSlots } = require('./utils/migrtateSlots/index.js');
 
+console.log('\x1b[35m Migration starting \x1b[0m');
+
+const FILES_TO_MIGRATE_PATH = './testFiles';
 const FILE_CONTENT_DELEGATES = {
-  migrateSlots
-}
-const filesPath = './testFiles';
+  migrateSlots,
+};
 
-const SummaryLog = {};
+let SummaryLog = {};
 
 function migrateFileContent(filePath) {
   const fileContent = fs.readFileSync(filePath, 'utf8');
 
   const fileContentToSave = Object.entries(FILE_CONTENT_DELEGATES)
-    .reduce((updatedFileContent, [delegateId, delegateMethod]) => {
+  .reduce((updatedFileContent, [delegateId, delegateMethod]) => {
       const { isApplied, fileContentModified } = delegateMethod(updatedFileContent);
       if (isApplied) {
-        SummaryLog[filePath] = [
-          ...(SummaryLog[filePath] || []),
-          delegateId
-        ]
+        SummaryLog[filePath] = [...(SummaryLog[filePath] || []), delegateId];
       }
       return fileContentModified;
-    }, fileContent)
+    },
+    fileContent,
+  );
 
-    fs.writeFileSync(filePath, fileContentToSave, 'utf8');
+  fs.writeFileSync(filePath, fileContentToSave, 'utf8');
 }
 
 function findVueFiles(dirPath) {
@@ -35,6 +34,7 @@ function findVueFiles(dirPath) {
   files.forEach((file) => {
     const filePath = path.join(dirPath, file);
     const filePathMetaData = fs.statSync(filePath);
+
     if (filePathMetaData.isDirectory()) {
       findVueFiles(filePath);
     } else if (path.extname(filePath) === '.vue') {
@@ -43,7 +43,7 @@ function findVueFiles(dirPath) {
   });
 }
 
-findVueFiles(filesPath);
+findVueFiles(FILES_TO_MIGRATE_PATH);
 
 fs.writeFile('migration-summary.json', JSON.stringify(SummaryLog, null, 2), (err) => {
   if (err) throw err;

341201c3d9a3d29f47117f028e57a173935eb233
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-11-29 19:16:56 +0000

    fix path

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+const path = require("path");
 const { migrateToVue3 } = require('./utils/index.js');
 
-const FILES_TO_MIGRATE_PATH = path.join(__dirname, '../', 'testFiles');
+const FILES_TO_MIGRATE_PATH = path.join(__dirname, 'testFiles');
 migrateToVue3(FILES_TO_MIGRATE_PATH);

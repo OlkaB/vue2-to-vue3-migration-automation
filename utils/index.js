c350ec87eff798a2578da66b4c296c9afabd5092
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-11-29 20:16:46 +0000

    code cleanups

--- utils/index.js
+++ utils/index.js
@@ -3,41 +3,38 @@ const path = require("path");
 const { FILES_TO_MIGRATE_EXTENSIONS, EXTENSION_PER_FILE_TYPE } = require("./FilesToMigrate.js");
 const { FILE_CONTENT_DELEGATES } = require("./FileContentDelegates");
 
-const FILE_ENCODING = 'utf8';
+const FILE_ENCODING = "utf8";
 let SummaryLog = {};
 
-function checkCanUseMigrateMethodOnFile(fileExtension, delegateMigrateFileTypes) {
-  return delegateMigrateFileTypes.find(
-    (migrateFileType) => EXTENSION_PER_FILE_TYPE[migrateFileType] === fileExtension,
+function migrateToVue3(filesToMigratePath) {
+  console.log("\x1b[35m Migration starting \x1b[0m");
+
+  if (!isValuePotentialSystemPath(filesToMigratePath)) {
+    console.error("Invalid files path", { filesToMigratePath });
+    return;
+  }
+
+  migrateFilesFromPath(filesToMigratePath);
+  saveSummaryLog(SummaryLog);
+
+  console.log("\x1b[32m Migration ended \x1b[0m", SummaryLog);
+}
+
+function isValuePotentialSystemPath(value) {
+  return (
+    typeof filesToMigratePath == "string" &&
+    filesToMigratePath === path.basename(filesToMigratePath)
   );
 }
 
-// TODO drop pure function in favour of speedy not drilling SummaryLog down
-function saveDataToSummaryLog (filePath, delegateId) {
-  SummaryLog[filePath] = [...(SummaryLog[filePath] || []), delegateId];
+function saveSummaryLog(log) {
+  fs.writeFile("migration-summary.json", JSON.stringify(log, null, 2), (err) => {
+    if (err) throw err;
+    console.log("\x1b[31m Migration summary saved to a file. \x1b[0m");
+  });
 }
 
-function migrateFileContent(filePath, fileExtension) {
-  const fileContent = fs.readFileSync(filePath, FILE_ENCODING);
-
-  const fileContentToSave = Object.entries(FILE_CONTENT_DELEGATES).reduce(
-    (updatedFileContent, [delegateId, { migrateMethod, migrateFileTypes }]) => {
-      const canUseMigrateMethod = checkCanUseMigrateMethodOnFile(fileExtension, migrateFileTypes);
-      if (!canUseMigrateMethod) return updatedFileContent;
-
-      const { isApplied, fileContentModified } = migrateMethod(updatedFileContent);
-      if (isApplied) {
-        saveDataToSummaryLog(filePath, delegateId)
-      }
-      return fileContentModified;
-    },
-    fileContent,
-  );
-
-  fs.writeFileSync(filePath, fileContentToSave, FILE_ENCODING);
-}
-
-function migrateFilesContent(dirPath) {
+function migrateFilesFromPath(dirPath) {
   const files = fs.readdirSync(dirPath);
 
   files.forEach((file) => {
@@ -46,33 +43,46 @@ function migrateFilesContent(dirPath) {
     const isDirectory = filePathMetaData.isDirectory();
 
     if (isDirectory) {
-      return migrateFilesContent(filePath);
+      return migrateFilesFromPath(filePath);
     }
     const fileExtension = path.extname(filePath);
     if (FILES_TO_MIGRATE_EXTENSIONS.includes(fileExtension)) {
-      migrateFileContent(filePath, fileExtension);
+      migrateFile(filePath, fileExtension);
     }
   });
 }
 
-const migrateToVue3 = (filesToMigratePath) => {
-  if (
-    typeof filesToMigratePath !== "string" ||
-    filesToMigratePath === path.basename(filesToMigratePath)
-  ) {
-    console.error("Invalid files path", { filesToMigratePath });
-    return;
-  }
-  console.log("\x1b[35m Migration starting \x1b[0m");
+function migrateFile(filePath, fileExtension) {
+  const fileContent = fs.readFileSync(filePath, FILE_ENCODING);
+  const fileContentToSave = updateFileContent(fileContent, fileExtension);
+  fs.writeFileSync(filePath, fileContentToSave, FILE_ENCODING);
+}
 
-  migrateFilesContent(filesToMigratePath);
+function updateFileContent(fileContent, fileExtension) {
+  return Object.entries(FILE_CONTENT_DELEGATES).reduce(
+    (updatedFileContent, [delegateId, { migrateMethod, migrateFileTypes }]) => {
+      const canUseMigrateMethod = checkCanUseMigrateMethodOnFile(fileExtension, migrateFileTypes);
+      if (!canUseMigrateMethod) return updatedFileContent;
 
-  fs.writeFile("migration-summary.json", JSON.stringify(SummaryLog, null, 2), (err) => {
-    if (err) throw err;
-    console.log("\x1b[31m Migration summary saved to a file. \x1b[0m");
-  });
+      const { isApplied, fileContentModified } = migrateMethod(updatedFileContent);
+      if (isApplied) {
+        saveDataToSummaryLog(filePath, delegateId);
+      }
+      return fileContentModified;
+    },
+    fileContent,
+  );
+}
 
-  console.log("\x1b[32m Migration ended \x1b[0m", SummaryLog);
-};
+function checkCanUseMigrateMethodOnFile(fileExtension, delegateMigrateFileTypes) {
+  return delegateMigrateFileTypes.find(
+    (migrateFileType) => EXTENSION_PER_FILE_TYPE[migrateFileType] === fileExtension,
+  );
+}
+
+// TODO drop pure function in favour of speedy not drilling SummaryLog down
+function saveDataToSummaryLog(filePath, delegateId) {
+  SummaryLog[filePath] = [...(SummaryLog[filePath] || []), delegateId];
+}
 
 module.exports = { migrateToVue3 };

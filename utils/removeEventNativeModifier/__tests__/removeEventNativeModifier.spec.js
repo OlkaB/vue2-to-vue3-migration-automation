142f88f32ef6c3142efb6ca29165e1d1eb1601fb
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-12-07 20:54:38 +0000

    removeEventNativeModifier tests and method done

--- utils/removeEventNativeModifier/__tests__/removeEventNativeModifier.spec.js
+++ utils/removeEventNativeModifier/__tests__/removeEventNativeModifier.spec.js
@@ -1,1 +1,19 @@
-// TODO
+const { removeEventNativeModifier, EVENT_NATIVE_REGEXP } = require('../index');
+const {
+  MigrateableEventNative,
+  NonMigrateableEventNative
+ } = require('./testData');
+
+ describe('test removeEventNativeModifier', () => {
+  test.each(MigrateableEventNative)('removeEventNativeModifier should remove .native modifier', async (fileContent) => {
+    const modifiedContent = removeEventNativeModifier(fileContent);
+
+    expect(modifiedContent).not.toEqual(fileContent);
+    expect(EVENT_NATIVE_REGEXP.test(modifiedContent)).toEqual(false);
+  });
+
+  test.each(NonMigrateableEventNative)('removeEventNativeModifier should not change file content ', async (fileContent) => {
+    const modifiedContent = removeEventNativeModifier(fileContent);
+    expect(modifiedContent).toEqual(fileContent);
+  });
+});
--- utils/removeEventNativeModifier/index.js
+++ utils/removeEventNativeModifier/index.js
@@ -1,7 +1,24 @@
 const EVENT_NATIVE_REGEXP = /@[^"'>]*(\.native\b)[^"'>]*/
 
+
 function removeEventNativeModifier (fileContent) {
-  // TODO
+  if(typeof fileContent !== 'string') return fileContent;
+
+  let fileContentModified = fileContent;
+  if(EVENT_NATIVE_REGEXP.test(fileContent)) {
+    fileContentModified = removeModifier(
+      fileContent,
+      EVENT_NATIVE_REGEXP
+    );
+  }
+  return fileContentModified
+};
+
+function removeModifier (fileContent, regex) {
+  return fileContent.replaceAll(
+    new RegExp(regex, "gm"),
+    ''
+  );
 }
 
 module.exports = {

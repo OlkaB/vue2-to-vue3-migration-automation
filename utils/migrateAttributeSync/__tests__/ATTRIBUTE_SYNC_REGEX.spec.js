c9092ce772825e23271368b259d745c6a791c50f
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-12-07 19:27:54 +0000

    ATTRIBUTE_SYNC_REGEX tests

--- utils/migrateAttributeSync/__tests__/ATTRIBUTE_SYNC_REGEX.spec.js
+++ utils/migrateAttributeSync/__tests__/ATTRIBUTE_SYNC_REGEX.spec.js
@@ -1,1 +1,15 @@
-// TODO
+const { ATTRIBUTE_SYNC_REGEX } = require('../index');
+const {
+  MigrateableAttributeSync,
+  NonMigrateableAttributeSync,
+} = require('./testData');
+
+describe('test ATTRIBUTE_SYNC_REGEX', () => {
+  test.each(MigrateableAttributeSync)('ATTRIBUTE_SYNC_REGEX should pass with strings containing valid :attribute.sync binding', async (caseString) => {
+    expect(ATTRIBUTE_SYNC_REGEX.test(caseString)).toBe(true);
+  });
+
+  test.each(NonMigrateableAttributeSync)('ATTRIBUTE_SYNC_REGEX should not pass with strings not containing valid :attribute.sync binding', async (caseString) => {
+    expect(ATTRIBUTE_SYNC_REGEX.test(caseString)).toBe(false);
+  });
+});
--- utils/migrateAttributeSync/__tests__/testData.js
+++ utils/migrateAttributeSync/__tests__/testData.js
@@ -1,1 +1,35 @@
-// TODO
+const MigrateableAttributeSync = [
+  `<UserName
+    :first-name.sync="first"
+    :last-name.sync="last"
+  />`,
+  `<Component
+    :name.sync="name"
+    :surname.sync="surname"
+  />`,
+  `<SomeForm
+    :age.sync="user.age"
+    :address.sync="user.address"
+  >...</SomeForm>`
+];
+
+const NonMigrateableAttributeSync = [
+  `<UserName
+    :first-name.syncs="first"
+    :last-name="last"
+  />`,
+  `<Component
+    :name="name"
+    :surname="surname"
+  />`,
+  `<SomeForm
+    :age.number="user.age"
+    :address="user.address"
+  >...</SomeForm>`,
+  '',
+];
+
+module.exports = {
+  MigrateableAttributeSync,
+  NonMigrateableAttributeSync,
+}

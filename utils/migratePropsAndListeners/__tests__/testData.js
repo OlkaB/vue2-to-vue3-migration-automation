37d1e92338fd675a29eeca16a18ef69f90420e2e
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-12-09 19:05:44 +0000

    $props $listeners testing strings

--- utils/migratePropsAndListeners/__tests__/testData.js
+++ utils/migratePropsAndListeners/__tests__/testData.js
@@ -57,7 +57,78 @@ const MigrateableListeners = [
 },`,
 ];
 
+const NonMigrateableProps = [
+  `<CvTextInput
+  ref="inputWrapper"
+  v-model="inputValue"
+  v-bind="$prop"
+  @blur="$emit('blur')"
+  v-bind="attr.$prop"
+  @focus="$emit('focus')"
+/>`,
+  `methods: {
+  onRemove() {
+    this.$emit('remove', this.props);
+  }
+},`,
+  `methods: {
+onRemove() {
+  this.$emit('remove', this.$attrs.$props);
+}
+},`,
+  `watch: {
+  $propss: {
+    deep: true,
+    handler() {
+      const newConfig = this.getSliderConfig();
+      if (isEqual(newConfig, this.config)) return;
+      this.config = newConfig;
+    }
+  },`,
+  `computed: {
+    commonPropsToBind() {
+      return omit($.props, ParameterSpecificProps);
+    }
+  }`,
+  `<CvTextInput
+  ref="inputWrapper"
+  v-model="inputValue"
+  v-bind="$attrs.$prop">`,
+  'const defaultLabelText = wrapper.vm.$prop.label;',
+];
+
+const NonMigrateableListeners = [
+  `<div
+  data-tabs
+  :class="cv-tab"
+  role="navigation"
+  v-bind="$attrs"
+  v-on="$listener"
+  @keydown.right.prevent.stop="onRight"
+  @keydown.left.prevent.stop="onLeft"
+>`,
+  `inputListeners() {
+  return {
+    ...$.listeners,
+    change: (event) => this.onChange(event)
+  };
+},`, `<div
+role="navigation"
+v-bind="$attrs.$listeners"
+>`,
+  `onTertiaryClick(ev) {
+  /** see cv-modal events */
+  this.$emit('tertiary-click');
+
+  if (!this.$listenerss['tertiary-click']) {
+    this.maybeHide(ev, 'tertiary-click');
+  }
+},`,
+];
+
 module.exports = {
   MigrateableProps,
   MigrateableListeners,
+  NonMigrateableProps,
+  NonMigrateableListeners,
 };

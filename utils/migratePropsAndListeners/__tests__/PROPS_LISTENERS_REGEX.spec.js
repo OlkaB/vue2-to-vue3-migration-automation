7f7d5d9d00f6f1f071df4c4dbf01de56ec7c9f38
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-12-09 19:09:00 +0000

    test cases for props/listeners regex

--- utils/migratePropsAndListeners/__tests__/PROPS_LISTENERS_REGEX.spec.js
+++ utils/migratePropsAndListeners/__tests__/PROPS_LISTENERS_REGEX.spec.js
@@ -1,1 +1,25 @@
-// TODO
+const { PROPS_LISTENERS_REGEX } = require('../index');
+const {
+  MigrateableProps,
+  MigrateableListeners,
+  NonMigrateableProps,
+  NonMigrateableListeners,
+} = require('./testData');
+
+describe('test PROPS_LISTENERS_REGEX', () => {
+  test.each(MigrateableProps)('PROPS_LISTENERS_REGEX should pass with strings containing $props syntax)', async (caseString) => {
+    expect(PROPS_LISTENERS_REGEX.test(caseString)).toBe(true);
+  });
+
+  test.each(MigrateableListeners)('PROPS_LISTENERS_REGEX should pass with strings containing $listeners syntax', async (caseString) => {
+    expect(PROPS_LISTENERS_REGEX.test(caseString)).toBe(true);
+  });
+
+  test.each(NonMigrateableProps)('PROPS_LISTENERS_REGEX should not pass with strings not containing $props syntax', async (caseString) => {
+    expect(PROPS_LISTENERS_REGEX.test(caseString)).toBe(false);
+  });
+
+  test.each(NonMigrateableListeners)('PROPS_LISTENERS_REGEX should not pass with strings not containing $listeners syntax', async (caseString) => {
+    expect(PROPS_LISTENERS_REGEX.test(caseString)).toBe(false);
+  });
+});

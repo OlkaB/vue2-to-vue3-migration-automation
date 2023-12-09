f5d061dbcd7b94ed2742b9881ce0dc2cea05b6f4
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-12-09 21:05:51 +0000

    test data prepared

--- utils/migrateLifecycleHooks/__tests__/testData.js
+++ utils/migrateLifecycleHooks/__tests__/testData.js
@@ -1,1 +1,34 @@
-// TODO
+const MigrateableStrings = [
+  `},
+  beforeDestroy() {
+    window.removeEventListener('resize', this.trackWindowWidth);
+  },`,
+  `},
+destroyed() {
+window.removeEventListener('resize', this.onResize);
+},`,
+];
+
+const NonMigrateableStrings = [
+  `this.$on('cv:beforeDestroy', (srcComponent) => this.onCvBeforeDestroy(srcComponent));`,
+  `},
+  onCvBeforeDestroy(srcComponent) {
+    const tabIndex = this.tabs.findIndex((item) => item.uid === srcComponent.uid);
+    if (tabIndex === -1) return;
+`,
+  `someValue.destroyed() {`,
+  `someValue.beforeDestroy() {`,
+  `if(false) beforeDestroy()`,
+  'beforeDestroy.someValue',
+  `detroyed() {
+    window.removeEventListener('resize', this.onResize);
+    },`,
+  `beforeDestroyy() {
+      window.removeEventListener('resize', this.onResize);
+      },`,
+];
+
+module.exports = {
+  MigrateableStrings,
+  NonMigrateableStrings,
+};

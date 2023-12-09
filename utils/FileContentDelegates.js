dfcbc22cf0c2a07f9b1bf777bac62dd96e6e2345
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-12-09 20:56:13 +0000

    lifecycle hooks migration wireframe

--- utils/FileContentDelegates.js
+++ utils/FileContentDelegates.js
@@ -5,6 +5,7 @@ const { migrateTranslations } = require('./migrateTranslations/index');
 const { removeEventNativeModifier } = require('./removeEventNativeModifier/index');
 const { migrateAttributeSync } = require('./migrateAttributeSync/index');
 const { migratePropsAndListeners } = require('./migratePropsAndListeners/index');
+const { migrateLifecycleHooks } = require('./migrateLifecycleHooks/index');
 
 exports.FILE_CONTENT_DELEGATES = {
   migrateSlots: {
@@ -32,5 +33,12 @@ exports.FILE_CONTENT_DELEGATES = {
   migratePropsAndListeners: {
     migrateMethod: migratePropsAndListeners,
     migrateFileTypes: [FILE_TYPES_FOR_MIGRATION.vue],
+  },
+  migrateLifecycleHooks: {
+    migrateMethod: migrateLifecycleHooks,
+    migrateFileTypes: [
+      FILE_TYPES_FOR_MIGRATION.vue,
+      FILE_TYPES_FOR_MIGRATION,
+    ],
   },
 };
---
+++ utils/migrateLifecycleHooks/index.js
@@ -0,0 +1,10 @@
+const LIFECYCLE_HOOKS_REGEX = /placeholder/;
+
+function migrateLifecycleHooks() {
+  // TODO
+}
+
+module.exports = {
+  LIFECYCLE_HOOKS_REGEX,
+  migrateLifecycleHooks,
+};

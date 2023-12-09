d5df741edb7f8d48b51cc0d279e44cc9565089d4
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-12-09 21:08:25 +0000

    lifecycle hooks test cases

--- utils/migrateLifecycleHooks/__tests__/LIFECYCLE_HOOKS_REGEX.spec.js
+++ utils/migrateLifecycleHooks/__tests__/LIFECYCLE_HOOKS_REGEX.spec.js
@@ -1,1 +1,15 @@
-// TODO
+const { LIFECYCLE_HOOKS_REGEX } = require('../index');
+const {
+  MigrateableStrings,
+  NonMigrateableStrings,
+} = require('./testData');
+
+describe('test LIFECYCLE_HOOKS_REGEX', () => {
+  test.each(MigrateableStrings)('LIFECYCLE_HOOKS_REGEX should pass with strings containing migrateable slot syntax)', async (caseString) => {
+    expect(LIFECYCLE_HOOKS_REGEX.test(caseString)).toBe(true);
+  });
+
+  test.each(NonMigrateableStrings)('LIFECYCLE_HOOKS_REGEX should not pass with strings not containing migrateable slot syntax', async (caseString) => {
+    expect(LIFECYCLE_HOOKS_REGEX.test(caseString)).toBe(false);
+  });
+});
